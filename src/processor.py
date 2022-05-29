e26259fe497210d953cfacc86adf46b563777b01
John <johnnybalcombe@gmail.com>
2022-05-29 19:44:02 +0200

    Added two csvs as lookup tables, wrote some dummy code to access them and retreive some data
    - Two lookup tables in csv form
    - Opened them and added some if statements to test in looking up emotion and looking up an action (all String based for now)

--- src/processor.py
+++ src/processor.py
@@ -1,9 +1,13 @@
+from cgitb import lookup
 import json
 import uuid
+import csv
+import numpy as np
 
 import rospy
 from std_msgs.msg import String
 from typing import List
+
 
 
 def deserialize(data):
@@ -56,12 +60,30 @@ class State:
     current_emotion = None
     current_action = None
 
+
+def update_current_emotion(intent):
+    currentEmotion = State.current_emotion
+
+
+
+
 def process_intent(intent):
-    # Look up current action
+
+
+    # OpenCSV and turn it into dictionary
+    input_file = csv.DictReader(open("lookup.csv"))
+
+    #GetCurrentEmotion
     emotion = State.current_emotion
 
+
     # Look up corresponding field in table
-    action_request = look_up_action(intent, emotion)
+    for key, value in input_file.items():
+        Name, Confidence, Sentiment, CorrespondingAction  = value
+    if (Name == "HappyGivePaw") & (Confidence == "1") & (Sentiment == "Happy") & (emotion == "Sad"):
+        # This line would correspond to the JSON or directly turning the String into a JSON
+        action = CorrespondingAction
+        action_request = look_up_action(action, intent, emotion)
 
     return action_request
 
@@ -69,7 +91,15 @@ class UUIDv4(uuid.uuid4):
     def __dict__(self):
         return str(self)
 
-def look_up_action(intent, emotion):
+def look_up_action(action, intent, emotion):
+    input_file = csv.DictReader(open("actionlookup.csv"))
+    for key, value in input_file.items():
+        Name, subcommand1 ,subcommand2 ,subcommand3 = value
+    if (Name == action):
+        print (ActionRequest (id=id, clear_action_queue=True, actions=[subcommand1, subcommand2, subcommand3])) 
+        # We need to have a fix for the classes to correspond to the table
+       
+
     id = uuid.uuid4()
     State.current_action = id
     return ActionRequest(id=id, clear_action_queue=False, actions=[SitAction(start_time=1000), GroupAction(actions=[SitAction(start_time=3000)])])
